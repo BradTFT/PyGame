143932998e70ba5427b8717181c6959c82397dbf
BradTFT <bradtft@outlook.com>
2022-05-26 13:34:47 -0400

    Move all tutorial files to /pygame-tutorial

--- pygame-tutorial/game.py
+++ pygame-tutorial/game.py
@@ -6,7 +6,14 @@ pygame.init()
 #display surface(window)
 screen = pygame.display.set_mode((800,400))
 #                     window width^    ^height of the window
+#sets window name
 pygame.display.set_caption('Game title here')
+#set framerate
+clock = pygame.time.Clock()
+
+#create a surface(line 26)
+testSurface = pygame.Surface((100, 200))
+testSurface.fill('Red')
 
 #draw all elements
 #runs window forever
@@ -18,7 +25,15 @@ while True:
             #sys exit to stop pygame error (video system not initalized)
             exit()
 
+    #!code to display assets/objects
+
+    #display surface(line 14)
+    #coordinates are "reversed" (picture a grid graph)
+    #origin point(0,0) is top left
+    screen.blit(testSurface,(0,0))
 
     #puts everything onto the display
     pygame.display.update()
+    #adds framerate to game
+    clock.tick(60)
 
