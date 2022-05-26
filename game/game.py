69ee7521c6e5a9ea71182f3f74af67f3caef004f
BradTFT <bradtft@outlook.com>
2022-05-26 13:04:13 -0400

    set up pygame Hello World

---
+++ game/game.py
@@ -0,0 +1,24 @@
+import pygame
+from sys import exit
+pygame.init()
+#import/initiate pygame
+
+#display surface(window)
+screen = pygame.display.set_mode((800,400))
+#                     window width^    ^height of the window
+pygame.display.set_caption('Game title here')
+
+#draw all elements
+#runs window forever
+while True:
+    #loops thru all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            #sys exit to stop pygame error (video system not initalized)
+            exit()
+
+
+    #puts everything onto the display
+    pygame.display.update()
+
